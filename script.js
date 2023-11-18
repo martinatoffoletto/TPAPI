da7246efdb810fb72e3bd5b6da7419e5f8bf3640
martinatoffoletto <matoffo@hotmail.com>
2023-11-18 19:37:37 -0300

    e

---
+++ script.js
@@ -0,0 +1,14 @@
+function showRegisterForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+}
+
+function login() {
+    // Aquí puedes agregar la lógica para procesar el inicio de sesión
+    console.log('Intento de inicio de sesión');
+}
+
+function register() {
+    // Aquí puedes agregar la lógica para procesar el registro
+    console.log('Intento de registro');
+}
