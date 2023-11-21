3463384be418a734c674457ee6f369e0a5c54e63
martinatoffoletto <matoffo@hotmail.com>
2023-11-21 07:59:21 -0300

    x

--- demo/src/main/resources/index.js
+++ demo/src/main/resources/index.js
@@ -1,14 +1,3 @@
-import { createRoot } from 'react-dom/client';
-
-function NavigationBar() {
-  // TODO: Actually implement a navigation bar
-  return <h1>Hello from React!</h1>;
-}
-
-const domNode = document.getElementById('navigation');
-const root = createRoot(domNode);
-root.render(<NavigationBar />);
-
 function showRegisterForm() {
     document.getElementById('login-form').style.display = 'none';
     document.getElementById('register-form').style.display = 'block';
